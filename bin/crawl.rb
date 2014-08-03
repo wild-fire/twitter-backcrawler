34ee2c8b7f9aa169cef8ec97812898bf9f321296
David Brenes <davidjbrenes@gmail.com>
2014-08-03 13:02:45 +0200

    Adding the config twitter file option to the command (and a file example)

--- bin/crawl.rb
+++ bin/crawl.rb
@@ -3,15 +3,18 @@
 require 'rubygems'
 require 'commander/import'
 require_relative '../lib/back_crawler.rb'
+require_relative '../lib/twitter_exception_notifier.rb'
 
 program :version, '0.0.1'
 program :description, 'Script for fetching old tweets'
- 
+
 command :search do |c|
   c.syntax = 'Twitter Backcrawler search tweet-id keywords'
   c.summary = 'It returns tweets before the tweets id containing the desired keywords'
   c.description = ''
+  c.option '--twitter_config_file path/to/config.yml', String, 'Path to twitter config file (default: config/twitter.yml)'
   c.action do |args, options|
+    options.default :twitter_config_file  => 'config/twitter.yml'
     if args.length != 2
       puts "Please, provide first tweet id and keywords"
     else
